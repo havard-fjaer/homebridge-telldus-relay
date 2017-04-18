f43a3d9fe6b8d056f9800759b80fb2de5c835f42
Håvard Fjær <git@fjaer.com>
2017-04-18 22:24:08 +0200

    Simpler server

--- server.js
+++ server.js
@@ -1,65 +1,31 @@
-#!/usr/bin/env node
-
 var amqp = require('amqplib/callback_api');
-var logger = require('logfmt');
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.globalAgent.maxSockets = Infinity;
+app.set('port', (process.env.PORT || 5000));
 
-function start() {
-    console.log({
-        type: 'info',
-        msg: 'starting server'
+
+
+
+app.get('/', function(request, response) {
+    response.write('test');
+    response.end();
+
+    amqp.connect(process.env.CLOUDAMQP_URL, function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            var q = 'hello';
+
+            ch.assertQueue(q, { durable: false });
+            // Note: on Node 6 Buffer.from(msg) should be used
+            ch.sendToQueue(q, new Buffer('Hello World!'));
+            console.log(" [x] Sent 'Hello World!'");
+        });
     });
 
-    var instance = app(config);
-    instance.on('ready', createServer);
-    instance.on('lost', abort);
-
-    function createServer() {
-        var server = http.createServer(function(req, res) {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('okay');
-        });
-
-        process.on('SIGTERM', shutdown);
-        instance
-            .removeListener('lost', abort)
-            .on('lost', shutdown);
-
-        server.listen(443, onListen);
+});
 
 
 
-
-        amqp.connect(process.env.CLOUDAMQP_URL, function(err, conn) {
-            conn.createChannel(function(err, ch) {
-                var q = 'hello';
-
-                ch.assertQueue(q, { durable: false });
-                // Note: on Node 6 Buffer.from(msg) should be used
-                ch.sendToQueue(q, new Buffer('Hello World!'));
-                console.log(" [x] Sent 'Hello World!'");
-            });
-        });
-    }
-
-    function onListen() {
-        console.log({ type: 'info', msg: 'listening', port: server.address().port });
-    }
-
-    function shutdown() {
-        console.log({ type: 'info', msg: 'shutting down' });
-        server.close(function() {
-            console.log({ type: 'info', msg: 'exiting' });
-            process.exit();
-        });
-    }
-
-
-    function abort() {
-        console.log({ type: 'info', msg: 'shutting down', abort: true });
-        process.exit();
-    }
-
-}
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
