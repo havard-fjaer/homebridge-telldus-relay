bc0cdf1bfedc112cd2e6e3a36a39dad33e78a352
Håvard Fjær <git@fjaer.com>
2017-04-18 22:36:13 +0200

    Jackrabit

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-var amqp = require('amqplib/callback_api');
 var express = require('express');
 var app = express();
 
+var jackrabbit = require('jackrabbit');
+var rabbit = jackrabbit(process.env.RABBIT_URL);
 app.set('port', (process.env.PORT || 5000));
-
-
-
 
 app.get('/', function(request, response) {
     console.log("test log");
     response.write('test');
     response.end();
-
+    rabbit
+        .default()
+        .publish('Hello World!', { key: 'hello' })
+        .on('drain', rabbit.close);
 });
-
-
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
