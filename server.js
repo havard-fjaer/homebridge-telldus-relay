619ec1d6dad6c9499f610bcf144c088e2b3d0a51
Håvard Fjær <git@fjaer.com>
2017-04-20 22:49:43 +0200

    Fixed express route

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var app = express();
 var redis = require('redis');
 var publisher = redis.createClient(process.env.REDIS_URL);
 var secret = process.env.APP_SECRET;
+
 app.set('port', (process.env.PORT));
 
-app.get('/:sensorType/:sensor/:action?secret=:secret', function(req, res) {
-    if (req.params['secret'] !== secret)
+app.get('/:sensorType/:sensor/:action', function(req, res) {
+    if (req.query.secret !== secret)
         res.send("Invalid secret!");
     else {
-        publisher.publish(req.params['sensorType'] + ":" + req.params['sensor'], req.params['action']);
+        publisher.publish(req.params.sensorType + ":" + req.params.sensor, req.params.action);
         res.send(req.params)
     }
 });
