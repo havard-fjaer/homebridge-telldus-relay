45c2596ed668d8f810c9f3fafcdf3773e5171fbd
Håvard Fjær <git@fjaer.com>
2017-04-20 22:41:56 +0200

    Added secret

--- receiver.js
+++ receiver.js
@@ -1,12 +1,22 @@
 var redis = require("redis"),
     subscriber = redis.createClient('redis://h:pc2150d5c99e50bc0a6c42ccf2da094fb6af3ef190303dc7269c487da2422e584@ec2-34-206-56-140.compute-1.amazonaws.com:25729');
 
-var channelName = "sensor:";
+var sensorTypes = ["sensor", "button", "switch"];
+
 
 subscriber.on("pmessage", function(pattern, channel, message) {
-    var sensor = channel.replace(channelName, '');
-    console.log("Pattern: " + pattern + " - Channel: " + channel + " - Sensor: " + sensor + " - Message: " + message);
-    sensorDetected({ sensor, action: message });
+
+
+    for (var i = 0, len = sensorTypes.length; i < len; i++) {
+        var replacePattern = sensorTypes[i] + ":";
+        if (channel.startsWith(replacePattern)) {
+            var sensor = channel.replace(replacePattern, "");
+            console.log("Pattern: " + pattern + " - Channel: " + channel + " - Sensor: " + sensor + " - Message: " + message);
+            sensorDetected({ sensor, action: message });
+        }
+    }
+
+
 
 });
 
@@ -14,4 +24,6 @@ function sensorDetected(obj) {
     console.log("Sensor: " + obj.sensor + " - Action: " + obj.action);
 }
 
-subscriber.psubscribe(channelName + "*");
+for (var i = 0, len = sensorTypes.length; i < len; i++) {
+    subscriber.psubscribe(sensorTypes[i] + ":*");
+}
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 var redis = require('redis');
 var publisher = redis.createClient(process.env.REDIS_URL);
-
+var secret = process.env.APP_SECRET;
 app.set('port', (process.env.PORT));
 
-app.get('/:sensorType/:sensor/:action', function(req, res) {
-    publisher.publish(req.params['sensorType'] + ":" + req.params['sensor'], req.params['action']);
-    res.send(req.params)
+app.get('/:sensorType/:sensor/:action?secret=:secret', function(req, res) {
+    if (req.params['secret'] !== secret)
+        res.send("Invalid secret!");
+    else {
+        publisher.publish(req.params['sensorType'] + ":" + req.params['sensor'], req.params['action']);
+        res.send(req.params)
+    }
 });
 
 app.listen(app.get('port'), function() {
